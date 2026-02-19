a5914339fc7d36125959dd4d2872d552406054a8
MatheusVianaAds <marketingmv06@gmail.com>
2026-02-18 21:27:51 -0300

    fix: ajuste hero banner

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,67 +1,58 @@
 import Container from "./Container";
 import Button from "./Button";
 import Typography from "./Typography";
-import bannerImg from "../assets/banner.png";
+import bannerImg from "../assets/banner-teste.webp";
 
 const HeroBanner = () => {
   return (
+    <section className="bg-black-gradient text-white-custom">
+      <Container className="pt-20 pb-8 lg:pb-0">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:items-center">
 
-    <section className="bg-gradient-to-r from-[#000000] to-[#1b1b1b]">
-      <Container>
-        <div
-          className="
-          flex 
-          flex-col-reverse 
-          lg:flex-row 
-          items-center 
-          justify-between 
-          gap-12
-          pb-8
-          lg:pb-0
-        "
-        >
+          {/* IMAGEM (aparece primeiro no mobile) */}
+          <div className="order-1 lg:order-2 flex justify-center lg:justify-end">
+            <img
+              src={bannerImg}
+              alt="Profissional"
+              className="w-full max-w-[800px] object-contain"
+            />
+          </div>
+
           {/* TEXTO */}
-          <div className="max-w-xl text-center md:text-left">
-
-            <Typography as="h1" className="text-white-custom">
+          <div className="order-2 lg:order-1 flex flex-col justify-center text-center lg:text-left">
+            <Typography
+              as="h1"
+              className="text-3xl md:text-5xl font-bold leading-tight"
+            >
               <span className="text-blue-custom">
                 Pare de viver na incerteza no fim do mês,
               </span>{" "}
               meu trabalho é ajudar você a aumentar seus agendamentos.
             </Typography>
 
-            <Typography as="p" className="mt-6 text-white-custom">
+            <Typography as="p" className="mt-6 text-lg text-gray-200 max-w-xl">
               Atuo como parceiro da sua empresa, tratando seus investimentos
               como se fossem meus, sempre buscando melhores resultados ao
               menor custo possível.
             </Typography>
 
+            <div className="mt-8 flex justify-center lg:justify-start">
 
-            <div className="mt-8 flex justify-center md:justify-start">
-              <Button className="w-[70%]">
-                <a
-                  href="https://form.respondi.app/XN5iIrID"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  FAÇA SEU ORÇAMENTO
-                </a>
-              </Button>
+              <a
+                href="https://api.whatsapp.com/send/?phone=5511975099618&text&type=phone_number&app_absent=0"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Button className="px-10 py-4 text-lg font-semibold cursor-pointer"> FAÇA SEU ORÇAMENTO</Button>
+
+              </a>
+
             </div>
           </div>
 
-          {/* IMAGEM */}
-          <div className="w-full md:w-1/2 flex justify-center md:justify-end md:shrink-0">
-            <img
-              src={bannerImg}
-              alt="Profissional"
-              className="h-[600px] lg:h-[650px] object-contain"
-            />
-          </div>
-        </div >
-      </Container >
+        </div>
+      </Container>
     </section>
-
   );
 };
 
