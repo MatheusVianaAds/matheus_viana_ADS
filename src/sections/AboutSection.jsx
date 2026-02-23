6c1c6f0030cf6c59fcd1b770ae79493b54ed8821
MatheusVianaAds <marketingmv06@gmail.com>
2026-02-23 12:23:47 -0300

    fix: ajuste img about

--- src/sections/AboutSection.jsx
+++ src/sections/AboutSection.jsx
@@ -10,7 +10,7 @@ export default function AboutSection() {
           {/* IMAGEM */}
           <div className="flex justify-center mx-auto lg:justify-start max-w-[400px] lg:max-w-full">
             <img
-              src="/about.svg"
+              src="/about.webp"
               alt="Matheus Viana"
               className="object-contain "
               loading="lazy"
