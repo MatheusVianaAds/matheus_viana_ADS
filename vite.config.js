7a1659ba82463aaf29b0b0b3ccc475ad7f259707
MatheusVianaAds <marketingmv06@gmail.com>
2026-02-24 20:07:54 -0300

    fix: ajuste otimização

--- vite.config.js
+++ vite.config.js
@@ -24,6 +24,7 @@ export default defineConfig({
       output: {
         manualChunks: {
           react: ["react", "react-dom"],
+          router: ["react-router-dom"],
         }
       }
     }
